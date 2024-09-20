917bc981faf28be0ff468fd762716c687312a463
charmingruby <mrrubycanal3105@gmail.com>
2024-09-20 15:17:11 -0300

    chore: adds hello lambda

--- function/hello/main.go
+++ function/hello/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"context"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/charmingruby/sinf/internal/infra/function"
+)
+
+func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	json, err := function.NewResponseBody("hello").Marshal()
+	if err != nil {
+		return function.NewResponse(http.StatusInternalServerError, []byte(err.Error())), err
+	}
+
+	return function.NewResponse(http.StatusOK, json), nil
+}
+
 func main() {
-	println("hello world")
+	lambda.Start(handler)
 }
