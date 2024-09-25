1003d10d76372ddffb033b7d33f8152caf79df98
charmingruby <mrrubycanal3105@gmail.com>
2024-09-24 23:18:03 -0300

    fix: fixes lambda iteration

--- terraform/modules/serverless/lambda.tf
+++ terraform/modules/serverless/lambda.tf
@@ -5,8 +5,8 @@ resource "null_resource" "build_lambda" {
     command = <<EOT
       mkdir -p bin
       mkdir -p dist
-      GOOS=linux GOARCH=amd64 go build -o bin/${each.value.lambda.name} function/${each.value.lambda.name}/main.go
-      zip -j dist/${each.value.lambda.name}.zip bin/${each.value.lambda.name}
+      GOOS=linux GOARCH=amd64 go build -o bin/${each.value.name} function/${each.value.name}/main.go
+      zip -j dist/${each.value.name}.zip bin/${each.value.name}
     EOT
   }
 }
@@ -16,17 +16,17 @@ resource "aws_s3_object" "lambda_zip_bucket" {
   depends_on = [null_resource.build_lambda]
 
   bucket = "lambdas-zip-bucket"
-  key    = "${var.s3_key_lambda_prefix}/${each.value.lambda.name}.zip"
-  source = "dist/${each.value.lambda.name}.zip"
+  key    = "${var.s3_key_lambda_prefix}/${each.value.name}.zip"
+  source = "dist/${each.value.name}.zip"
 }
 
 resource "aws_lambda_function" "lambda" {
   for_each   = { for idx, lambda in var.lambdas : idx => lambda }
   depends_on = [aws_s3_object.lambda_zip_bucket]
 
-  function_name = each.value.lambda.name
-  s3_bucket     = aws_s3_object.lambda_zip_bucket[each.value.lambda.name].bucket
-  s3_key        = aws_s3_object.lambda_zip_bucket[each.value.lambda.name].key
+  function_name = each.value.name
+  s3_bucket     = aws_s3_object.lambda_zip_bucket[each.value.name].bucket
+  s3_key        = aws_s3_object.lambda_zip_bucket[each.value.name].key
   handler       = "main"
   runtime       = "go1.x"
   role          = data.aws_iam_policy_document.lambda_assume_role
